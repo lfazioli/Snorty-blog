1f1efcaf2c00a11df258588723378051147abe09
Snorty <53367242+lfazioli@users.noreply.github.com>
2025-12-02 19:41:12 +0100

    fix: remove bug and create Post

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import PostPage from "./pages/Post";
 import About from "./pages/about";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post/:slug" element={<PostPage />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
