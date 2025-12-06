0e44308c7fa543461ba66404c4bff627d5db46f0
Snorty <53367242+lfazioli@users.noreply.github.com>
2025-12-06 10:34:31 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { HashRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
 import Posts from "./pages/Posts";
 import PostPage from "./posts/Post1";
@@ -12,32 +11,24 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
-
 import { AuthProvider } from "./context/AuthContext";
 
-// Mantine (obbligatorio)
-import { MantineProvider } from "@mantine/core";
-
-// Mantine Window CSS
-import "@gfazioli/mantine-window/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MantineProvider defaultColorScheme="dark">
-      <AuthProvider>
-        <HashRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/post/:slug" element={<PostPage />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/Posts" element={<Posts />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/reset-password/:token" element={<ResetPassword />} />
-          </Routes>
-        </HashRouter>
-      </AuthProvider>
-    </MantineProvider>
+    <AuthProvider>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/post/:slug" element={<PostPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/Posts" element={<Posts />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+        </Routes>
+      </HashRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
